5ef39d800dce3bc9e883c3e7a229aa8d8cf8c4ba
Dean Ellis <dellis1972@googlemail.com>
2026-08-18 21:10:33 +0100

    Add Docker build. Use vulkan-sdk-1.4.350.0 spirvcross

--- build/BuildLinuxTask.cs
+++ build/BuildLinuxTask.cs
@@ -13,8 +13,8 @@ public sealed class BuildLinuxTask : FrostingTask<BuildContext>
         Directory.CreateDirectory(buildWorkingDir);
         // Path relative to the buildWorkingDir
         var cmakeListsPath = System.IO.Path.Combine("..", "spirvcross", "CMakeLists.txt");
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = $"-DCMAKE_BUILD_TYPE=Release {cmakeListsPath}" });
-        context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "" });
+        context.StartProcessWithDocker("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = $"-DCMAKE_BUILD_TYPE=Release {cmakeListsPath}" });
+        context.StartProcessWithDocker("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "" });
         var files = Directory.GetFiles(buildWorkingDir, "spirv-cross", SearchOption.TopDirectoryOnly);
         context.CopyFile(files[0], $"{context.ArtifactsDir}/spirv-cross");
     }
